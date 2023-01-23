90565b4e280d64e2e18cee1f7201183d71dcfa7a
fernando predes <fernando_predes@hotmail.com>
2023-01-23 15:46:30 -0300

    rodapé e navbar

---
+++ src/components/FooterItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>footer</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "FooterItem",
+};
+</script>
---
+++ src/components/NavbarItem.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="nav">
+    <router-link to="/">
+      <img src="/img/patilogo.ong" alt="pati">
+    </router-link>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
