6e8a0c021af776b060540ac33b9d6c3632999818
Fernando Predes <fernando_predes@hotmail.com>
2023-01-24 11:18:52 -0300

    finalizada a navbar

--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -3,7 +3,56 @@
     <router-link to="/">
       <img src="/img/patilogo.ong" alt="pati">
     </router-link>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
+    <h2 id="nav-title">Pati</h2>
+    <div id="nav" class="links">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/about">Entrar</router-link>
+      <router-link to="/about">Cadastrar</router-link>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+export default {
+  name: "NavbarItem",
+};
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  border-bottom: 1px solid #eee;
+}
+#nav a {
+  text-decoration: none;
+  margin-right: 15px;
+}
+#nav img {
+  width: 35px;
+}
+#nav-title {
+  font-size: 3rem;
+  font-weight: 300;
+}
+#logo-container,
+#nav-links {
+  width: 400px;
+}
+#nav-links {
+  display: flex;
+  justify-content: flex-end;
+}
+button {
+  background-color: #FFFFFF;
+  border: none;
+  font-size: 16px;
+  cursor: pointer;
+}
+button:hover {
+  color: #c1b696;
+}
+</style>
